8841564112450cc402b52d8098a1643f9529c42e
Marek <s.marekszwed@gmail.com>
2024-10-19 23:00:20 +0200

    Added start files

---
+++ src/globalStyles.tsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    width: 100vw;
+    background-color: #141414;
+  }
+`;
+
+export default GlobalStyles
